20befda3fe33d23a451e2f326cbbb29f50ee7bac
Aaron <aarocka@protonmail.com>
2020-09-23 14:38:08 -0400

    added black fields check

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -2,11 +2,13 @@ package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
     EditText nameField;
@@ -30,7 +32,27 @@ public class FormActivity extends AppCompatActivity {
         submissionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                //check if user has entered required fields
+                if (nameField.getText().toString().matches("")||
+                        emailField.getText().toString().matches("")||
+                        passwordField.getText().toString().matches("")||
+                        passwordField2.getText().toString().matches("")
+                ){
+                    Context context = getApplicationContext();
+
+                    CharSequence text = "Ensure all required forms are filled";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {//forms must be filled to get to this block of code
+                    //check if password and password2 match
+                    if (passwordField.getText().toString().matches(passwordField2.getText().toString())) {
+
+                    }else {//runs if the password fields mismatch
+
+                    }
+                }
+
             }
         });
 
